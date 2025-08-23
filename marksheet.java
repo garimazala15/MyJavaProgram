fb52118dd0a9c0a3f945b45ece8b10b651032777
garimazala15 <garima1582000@gmail.com>
2025-08-23 18:04:59 +0530

    Add files via upload

---
+++ marksheet.java
@@ -0,0 +1,37 @@
+class MarkSheet{
+           public static void main(String []args){
+                       System.out.print("=========================================================================\n");
+                       System.out.print("|\t\t\t\tRGPV-BHOPAL\t\t\t\t|\n");
+                       System.out.print("=========================================================================\n");
+
+                       System.out.print("| Roll No\t:1810165\t\tCourse:MCA\t\t\t|\n");
+                       System.out.print("| Name\t\t:Garima Zala\t\tBranch:Computer Science\t\t|\n");
+                       System.out.print("| Father Name\t:Pradeep Zala\t\tSemester:III\t\t\t|\n");
+                       System.out.print("| Mother's Name\t:Laxmi Zala\t\tYear:2\t\t\t\t|\n");
+
+                      System.out.print("|                          ____________                                 |\n");
+                      System.out.print("|                         |            |                                |\n");
+                      System.out.print("|                         |   PHOTO    |                                |\n");
+                      System.out.print("|                         |            |                                |\n");
+                      System.out.print("|                         |            |                                |\n");               
+                      System.out.print("|                         |____________|                                |\n");
+                      System.out.print("=========================================================================\n");
+
+                     System.out.print("| SubCode\tSubName\t\t\tMarks\tMinMarks\tObtMarks|\n");
+                     System.out.print("-------------------------------------------------------------------------\n");
+                     System.out.print("| MCA-301\tData Structures\t\t100\t35\t\t 75     |\n");
+                     System.out.print("| MCA-302\tDBMS\t\t\t100\t35\t\t 80     |\n");
+                     System.out.print("| MCA-303\tOperating System\t100\t35\t\t 60     |\n");
+                     System.out.print("| MCA-304\tComputer Networks\t100\t35\t\t 80     |\n");
+                     System.out.print("| MCA-305\tSoftware Engg.\t\t100\t35\t\t 85     |\n");
+                     System.out.print("-------------------------------------------------------------------------\n");
+
+                     System.out.print("| Total Marks:\t500\t\tObtained:\t380\t\tPASS    |\n");
+                     System.out.print("=========================================================================\n");
+
+
+
+                      
+
+     }              
+}
