94a277d86b44a460698709896093a86c2739925b
garimazala15 <garima1582000@gmail.com>
2025-08-23 18:04:38 +0530

    Add files via upload
    BioData Code using simple java

---
+++ biodata.java
@@ -0,0 +1,35 @@
+class BioData{
+        public static void main(String []args){
+                   System.out.print("\t\tBIODATA\n\n");
+                   System.out.print("Name:\t\t\tGarima Zala\n");
+                   System.out.print("Father's Name:\t\tMr.Pradeep Zala\n");
+                   System.out.print("Mother's Name:\t\tMrs.Laxmi Zala\n");
+                   System.out.print("Date of Birth:\t\t15-08-2020\n");
+                   System.out.print("Gender:\t\t\tFemale\n");
+                   System.out.print("Marital Status:\t\tSingle\n");
+                   System.out.print("Nationality:\t\tIndian\n");
+                   System.out.print("Address:\t\t1278/5 Nanda Nagar,Indore(m.p)\n");
+                   System.out.print("Phone No:\t\t9098673927\n");
+                   System.out.print("Email:\t\t\tgarima1582000@gmail.com\n\n");
+
+
+                   System.out.print("EducationQualififcation:\n");
+                   System.out.print("\t-Master's of Computer Application,SCSIT,DAVV,2023\n");
+                   System.out.print("\t-12th(PCM),St.Joseph's School,2018\n\n");
+
+                   System.out.print("Skills:\n");
+                   System.out.print("\t-Java\n");
+                   System.out.print("\t-SpringBoot\n");
+                   System.out.print("\t-Angular\n");
+                   System.out.print("\t-SQL\n\n");
+
+                  System.out.print("Declaration:\n");
+                  System.out.print("\tI hereby declare that the information provided above are true to the best of my knowledge.\n\n");
+
+                  System.out.print("Place:\tIndore\n");
+                  System.out.print("Date:\t23-08-2025\n\n");
+                  System.out.print("Signature:\t_______________________\n");
+
+
+    }        
+}
